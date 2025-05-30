1d4b96dda0909a9f1bb3c71c89ba2c096a99c567
Jack Hawkins <jack.m.hawkins@vanderbilt.edu>
2025-05-30 13:38:19 -0500

    final-order-endpoints-added

--- Models/Order.cs
+++ Models/Order.cs
@@ -6,9 +6,9 @@ public class Order
     public DateTime CreatedAt { get; set; }
     public decimal Tip { get; set; }
     public int OrderTakerId { get; set; }
-    public Employee OrderTaker { get; set; }
+    public Employee? OrderTaker { get; set; }
     public int DelivererId { get; set; }
-    public Employee Deliverer { get; set; }
+    public Employee? Deliverer { get; set; }
     public int TableNum { get; set; }
     public bool IsDelivered { get; set; }
     public List<Pizza> Pizzas { get; set; }
